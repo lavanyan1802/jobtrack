5e601902728835e3d52b3391f1645112f5bbaffa
Lavanya Narayana <90672199+lavanyan1802@users.noreply.github.com>
2024-01-04 05:41:55 +0530

    Add files via upload

---
+++ build/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-NaSl_Vn65AyzqjBIbSv63fz17KedTvo",
+  authDomain: "jobtracker-e8a5f.firebaseapp.com",
+  projectId: "jobtracker-e8a5f",
+  storageBucket: "jobtracker-e8a5f.appspot.com",
+  messagingSenderId: "1004304569006",
+  appId: "1:1004304569006:web:d0e38b1bacbf911ab7533f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
